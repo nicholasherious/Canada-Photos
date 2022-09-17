f0c42f5418701845d8da9bec7640417f1f1f17af
nicholasherious <nherious@gmail.com>
2022-09-17 02:08:30 -0700

    fixing port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = 3000;
 
 import { getPhoto } from './services/unsplash.js';
 
@@ -9,6 +9,6 @@ import { getPhoto } from './services/unsplash.js';
 
 setInterval(getPhoto, 5 * 60 * 60 * 1000);
 
-app.listen(PORT, () => {
-  console.log(`Canada Photos listening on port ${PORT}`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Canada Photos is Running`);
 });
