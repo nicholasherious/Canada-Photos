06d313b888717b593d7539c670f986c26749def0
nherious <nherious@gmail.com>
2022-10-19 13:35:37 -0700

    changed res send

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || PORT;
 
-import { getPhoto } from './services/unsplash.js';
+import { getPhoto } from "./services/unsplash.js";
 
 // getPhoto()
 // postTweet()
 
 setInterval(getPhoto, 5 * 60 * 60 * 1000);
 
-app.get('/', (req, res) => {
-  res.send('What are you doing?');
+app.get("/", (req, res) => {
+  res.send("What are you doing here?");
 });
 
 app.listen(PORT, () => {
