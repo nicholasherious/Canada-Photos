699b8750e88b9ff5dd54998f09ce1cbe365dc1a2
nherious <nherious@gmail.com>
2022-10-22 03:13:49 -0700

    fixed scheduling

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || PORT;
 
-import { getPhoto } from "./services/unsplash.js";
+import { getPhoto } from './services/unsplash.js';
 
 // getPhoto()
 // postTweet()
 
-setInterval(getPhoto, 5 * 60 * 60 * 1000);
+const timer = () => {
+  getPhoto();
+  setTimeout(timer, 5 * 60 * 60 * 1000);
+};
 
-app.get("/", (req, res) => {
-  res.send("What are you doing here?");
+timer();
+
+app.get('/', (req, res) => {
+  res.send('What are you doing here?');
 });
 
 app.listen(PORT, () => {
