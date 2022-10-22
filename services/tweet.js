6a66699eed366a783d87868b3cd5e194fe7a123d
nherious <nherious@gmail.com>
2022-10-22 03:41:12 -0700

    add lodash to truncate tweet title

--- services/tweet.js
+++ services/tweet.js
@@ -1,4 +1,5 @@
 import { TwitterApi, EUploadMimeType } from 'twitter-api-v2';
+import { truncate } from 'lodash'
 import { config } from '../environment.js';
 
 const client = new TwitterApi({
@@ -14,7 +15,10 @@ export const postTweet = async (userInfo, buffer) => {
       client.v1.uploadMedia(buffer, { mimeType: EUploadMimeType.Jpeg }),
     ]);
 
-    let tweetTitle = userInfo?.description;
+    const title = userInfo?.description
+
+    let tweetTitle = truncate(title, {'length': 100, 'separator': ' '})
+
 
     if (tweetTitle === null) {
       tweetTitle = userInfo.location.title;
@@ -30,7 +34,7 @@ export const postTweet = async (userInfo, buffer) => {
 
     console.log(mediaIds);
     const { data: createdTweet } = await client.v2.tweet(
-      `${tweetTitle} | credit: ${userInfo.user.name} #photography #canada #photos #nature`,
+      `${tweetTitle} | credit: ${userInfo.user.name} #photography`,
       { media: { media_ids: mediaIds } }
     );
     console.log('Tweet', createdTweet.id, ':', createdTweet.text);
