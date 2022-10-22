ab87d4d7b87b84a5eb5120edcd52e611fa3ba3f1
nherious <nherious@gmail.com>
2022-10-22 03:17:41 -0700

    fixed undefined issues

--- services/tweet.js
+++ services/tweet.js
@@ -1,5 +1,5 @@
 import { TwitterApi, EUploadMimeType } from 'twitter-api-v2';
-import { config } from '../environment.js'
+import { config } from '../environment.js';
 
 const client = new TwitterApi({
   appKey: config.appKey,
@@ -22,6 +22,10 @@ export const postTweet = async (userInfo, buffer) => {
 
     if (tweetTitle === null) {
       tweetTitle = userInfo.user.location;
+    }
+
+    if (tweeTitle === undefined) {
+      tweetTitle = 'Canada is Amazing!';
     }
 
     console.log(mediaIds);
